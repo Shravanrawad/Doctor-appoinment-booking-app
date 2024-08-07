b822e58e579767390ac26d980e1f3ca8e786019a
shravanrawas <shravanrawas8@gmail.com>
2024-08-07 12:27:24 +0530

    changes

--- middleware.js
+++ middleware.js
@@ -2,9 +2,9 @@ import { NextResponse } from 'next/server'
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export async function middleware(request) {
-  const { isAuthenticated } = await getKindeServerSession(request);
+  const { isAuthenticated } = getKindeServerSession(request);
   
-  if (!isAuthenticated) {
+  if (!(await isAuthenticated())) {
     const loginUrl = new URL('/api/auth/login', request.url);
     loginUrl.searchParams.set('?post_login_redirect_url/', request.url);
     return NextResponse.redirect(loginUrl);
